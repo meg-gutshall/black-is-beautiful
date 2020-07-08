888356152e2ac4d8eb3301effc7b739d0502d10a
Meg Gutshall <meghan.gutshall@gmail.com>
2020-07-08 00:59:51 -0400

    Add Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
 
 // Test load code
 
 function component() {
   const element = document.createElement('div');
+  element.setAttribute('class', 'container');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
