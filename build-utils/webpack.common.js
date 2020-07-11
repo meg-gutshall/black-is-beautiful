6d0514a93fdebd7a6b1db9c43571fa1be12ff05e
Meg Gutshall <meghan.gutshall@gmail.com>
2020-07-11 15:33:47 -0400

    Remove Netlify config

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -66,7 +66,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, '../', 'dist'),
-    publicPath: '/black-is-beautiful',
+    publicPath: '/',
     filename: '[name].bundle.js'
   },
   devServer: {
