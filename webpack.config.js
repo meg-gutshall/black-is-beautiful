c3a2522c2d4404c4c09cf4e14ed9af1bd21ccb37
Meg Gutshall <meghan.gutshall@gmail.com>
2022-01-16 17:07:22 -0500

    Update configs

--- webpack.config.js
+++ webpack.config.js
@@ -16,43 +16,44 @@ module.exports = (env, argv) => {
             {
               loader: 'css-loader',
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             'resolve-url-loader',
             {
               loader: 'sass-loader',
               options: {
                 implementation: require('sass'),
-                sourceMap: true
-              }
-            }
+                sourceMap: true,
+              },
+            },
           ],
-        }, {
+        },
+        {
           test: /\.(png|jpg|gif)$/i,
           use: {
             loader: 'file-loader',
-          }
-        }, {
+          },
+        },
+        {
           test: /\.(ttf|svg|otf|eot|woff(2)?)$/i,
           use: {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        }, {
+              outputPath: 'fonts/',
+            },
+          },
+        },
+        {
           test: /\.js$/i,
           exclude: /node_modules/,
-          use: [
-            'babel-loader'
-          ]
-        }
-      ]
+          use: ['babel-loader'],
+        },
+      ],
     },
     resolve: {
-      extensions: ['*', '.js']
+      extensions: ['*', '.js'],
     },
     plugins: [
       new WebpackManifestPlugin(),
@@ -61,9 +62,9 @@ module.exports = (env, argv) => {
         template: 'black-is-beautiful.html',
         scriptLoading: 'defer',
         meta: {
-          viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
-        }
-      })
+          viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+        },
+      }),
     ],
     output: {
       path: path.resolve(__dirname, '../', 'dist'),
@@ -77,6 +78,6 @@ module.exports = (env, argv) => {
       compress: true,
       port: 9000,
       open: true,
-    }
+    },
   };
 };
